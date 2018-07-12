28331a7e7b4809bd538f64c60ce88c53f17f6ee5
alex_rocha_10sistemas <alex.rocha@10sistemas.com>
2018-07-12 08:31:34 -0300

    register

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-            .register("./service-worker.js")
+            .register("/js/service-worker.js")
             .then(function (reg) {                                
                 console.log(reg)
             })
