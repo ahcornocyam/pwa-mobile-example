b43f67cd786900cdc9fcde5a6ffde18b71b048b4
alex_rocha_10sistemas <alex.rocha@10sistemas.com>
2018-07-12 08:50:47 -0300

    dando uma olhada nos catch

--- service-worker.js
+++ service-worker.js
@@ -12,6 +12,7 @@ self.addEventListener("install", function (e) {
     e.waitUntil(caches.open(cacheName).then(function (cache) {
         return cache.addAll(filesToCache);
     }).catch(function (err) {
+        console.log('erro no install')
         console.log(err)
     }));
 });
@@ -21,6 +22,7 @@ self.addEventListener("fetch", function (e) {
             caches.match(e.request).then(function (response) {
         return response || fetch(e.request);
     }).catch(function (err) {
+        console.log('erro fetch', e.request)
         console.log(err)
     })
             );
