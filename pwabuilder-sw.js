dfd99812386deeaafa67179ad485c7b693012113
alex_rocha_10sistemas <alex.rocha@10sistemas.com>
2018-07-12 16:17:53 -0300

    voltando

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -18,7 +18,7 @@ self.addEventListener('fetch', function(event) {
     return caches.open('pwabuilder-offline').then(function (cache) {
       return fetch(request).then(function (response) {
         console.log('[PWA Builder] add page to offline'+response.url)
-        return cache.post(request, response);
+        return cache.put(request, response);
       });
     });
   };
